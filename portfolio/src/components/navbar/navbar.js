f06dca86ca81a05da32528eecab552ddfc7ff172
fnurrahmah125 <fnurrahmah125@gmail.com>
2023-11-23 17:06:33 +0700

    🎨 update footer and navbar component

--- portfolio/src/components/navbar/navbar.js
+++ portfolio/src/components/navbar/navbar.js
@@ -4,6 +4,13 @@ function initMenuToggle() {
   $(".header-toggle").click(function () {
     $(".header-nav").toggleClass("header-nav--open", 500);
     $(this).toggleClass("open");
+    $("main").toggleClass("menu-open");
+  });
+
+  $(window).on("resize", function () {
+    if ($(window).width() >= 768) {
+      $("main").removeClass("menu-open");
+    }
   });
 }
 
