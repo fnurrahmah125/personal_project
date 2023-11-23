51d25e4e0a4c37c503752d12b79799e168993cc4
fnurrahmah125 <fnurrahmah125@gmail.com>
2023-11-23 17:08:58 +0700

    🚚 update and rename scss files

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 import * as bootstrap from "bootstrap";
 
 // styles import
-import "./style/main.scss";
+import "./style/styles.scss";
 
 import initMenuToggle from "./components/navbar/navbar";
 
