ac46c1d849c1a607b6c37ea53beaebf32fe1ee94
fnurrahmah125 <fnurrahmah125@gmail.com>
2023-12-23 19:39:14 +0700

    💄 update notes and add button styling

--- notes-app/src/components/notes/NotesEditBtn.jsx
+++ notes-app/src/components/notes/NotesEditBtn.jsx
@@ -5,7 +5,7 @@ function NotesEditBtn({ id, onEditNote }) {
       className="tooltip cursor-pointer relative"
       onClick={() => onEditNote(id)}
     >
-      <span className="icon-pencil text-lg"></span>
+      <span className="icon-pencil text-xl"></span>
       <span className="tooltip-text bg-slate-800 text-white text-xs font-light py-1 px-4 rounded-md absolute -right-8 bottom-full w-max dark:bg-white dark:text-slate-800">
         Edit note
       </span>
